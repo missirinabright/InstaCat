f73a35c0e86ed1e64f91973b162159b8619e138f
github username <github email address>
2019-08-21 11:16:08 -0400

    adds post to user UL

--- InstaCat/app/serializers/user_serializer.rb
+++ InstaCat/app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :breed
+  has_many :posts
 end
--- main.js
+++ main.js
@@ -1,78 +1,121 @@
 //==================const==============================
 const URLposts = "http://localhost:3000/posts"
 const URLusers = "http://localhost:3000/users"
+const URLcomments = "http://localhost:3000/comments"
 
 const UL = document.querySelector("#list")
+const divPanel = document.querySelector("#list-panel")
+let postLI = document.createElement("postLI")
+
 //==================fetch==============================
 //---------fetch for GET-------------------------------
-fetch(URLposts)
-.then(response => response.json())
-.then(slapPostsToTheDOM)
+// fetch(URLposts)//`${userIdforPost}`)
+// .then(response => response.json())
+// .then(slapPostsToTheDOM)
 //---------end of fetch for GET------------------------
 
 //----------fetch for User-----------------------------
 // function getUsers() {
-  fetch(URLusers)
-  .then(response => response.json())
-  .then(slapUsersToTheDOM)
+fetch(URLusers)
+.then(response => response.json())
+.then(slapUsersToTheDOM)
+
+//----------fetch for Comment-----------------------------
+// function getUsers() {
+// fetch(URLcomments)
+// .then(response => response.json())
+// .then(slapCommentsToTheDOM)
+
 
 //----------end of fetch for User----------------------
 //---------fetch for POST------------------------------
-// fetch(URL), {
-//   method: 'POST',
-//   headers: {
-//     'Content-type': 'application/json'
-//     // 'Accept': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     title: newPost,
-//     img_url: newImgURL,
-//   })
-//   .then(response => response.json())
-//   .then(console.log("Hiii"))
-// } I don't need any CRUD for Post
+
+function createNewPost(newPost, newImgURL) {
+      fetch(URLposts), {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          title: newPost,
+          img_url: newImgURL
+        })
+        .then(response => response.json())
+        .then(console.log("Hiii"))
+      }
+}
+// I don't need any CRUD for Post
 //questions: how do I define newPost and newImgURL?
 // I created a form, and not seeing it on the page. How do I get that onto the page?
 
 //-----------new post form--------------------------------
-// function createnewpost(user_id) {
-//   // console.log(userId)
-//   // let form =document.createElement("form")
-//       let form = document.createElement("form")
-//       form.className = "new-post-form"
-//       form.userid = `${user_id}`
-//       form.innerHTML = `
-//       <input name="post" type="text" class="new-post" id="postinput" >
-//       <input name="img_url" type="text" class="img_url" id="img_urlinput" >
-//       <button type="submit" class="btn btn-primary">Submit</button>
-//       `
-//       postdivform.append(form);
-//     }
+function createnewpostform(user_id) {
+  // console.log(userId)
+  // let form =document.createElement("form")
+      let form = document.createElement("form")
+      form.className = "new-post-form"
+      form.userid = `${user_id}`
+      form.innerHTML = `
+      <input name="post" type="text" class="new-post" id="postinput" >
+      <input name="img_url" type="text" class="img_url" id="img_urlinput" >
+      <button type="submit" class="btn btn-primary">Submit</button>
+      `
+      append(form);
+    }
 //---------end of fetch for POST------------------------
 //====================slap==============================
-//---------slap for GET posts---------------------------------
-function slapPostsToTheDOM(posts) {
-      posts.forEach(post => {
-        let li = document.createElement("li")
-        li.innerHTML = `
-          <p> Me az tha ${post.title} </p>
-          <img src= "${post.img_url}"></img>`
-        UL.append(li)
-        console.log("Moo1")
-      })
-    }
-//---------end of slep for GET posts--------------------------
 //---------slap for GET users---------------------------------
 function slapUsersToTheDOM(users) {
   console.log("Moo2")
     users.forEach(user => {
-      let li = document.createElement("lil")
-      li.innerHTML =
-      `<p> ${user.name} of ${user.breed} </p>`
-      UL.append(li)
-      console.log("Moo3")
+      let userLI = document.createElement("userLI")
+      userLI.innerHTML =`
+      <p> Username: ${user.name} of ${user.breed} </p>
+      <button type="submit" class="btn btn-primary">Create New Post</button>`
+      // createnewpost(user.id)
+      UL.append(userLI)
+
+
+
+      user.posts.forEach(post => {
+        let postLI = document.createElement("postLI")
+        console.log(postLI)
+        postLI.innerHTML +=`
+        <p> Me az tha ${post.title} </p>
+        <img src= "${post.img_url}"></img>`
+          userLI.append(postLI)
+      // slapPostsToTheDOM(user.posts)
+      })
+    })
+  }
+
+// function slapPostsToTheDOM(posts) {
+//
+//       posts.forEach(post => {
+//
+//         let postLI = document.createElement("postLI")
+//         console.log(postLI)
+//         postLI.innerHTML += `
+//           <p> Me az tha ${post.title} </p>
+//           <img src= "${post.img_url}"></img>`
+//           userLI.append(postLI)
+//         // userLI.append(postLI)
+//         console.log("Moo1")
+//       })
+//     }
+
+function slapCommentsToTheDOM(comments) {
+  comments.forEach(comment => {
+    console.log("Test1")
   })
+
 }
+    // postLI.innerHTML = `
+    //   <p> Me az tha ${post.title} </p>
+    //   <img src= "${post.img_url}"></img>`
+    //Question: how do I add the post to each user?
+    //I need to put postLI inside of the userLI
 //---------end of slap for GET users--------------------------
 
 
@@ -81,6 +124,7 @@ function slapUsersToTheDOM(users) {
 
 //-----------------addEventListener!---------
 //-----------------inventory of what I need to create!!
+// Add event listener to the User;s Create New Post button so that once it's clicked, a form with new post will show
 //   CRUD for 2 models
 //   I have 3 models: User, Post and Comment
 //   So, I do CRUD for User and Comment. I don't really touch the POST, except for rendering it on the page
